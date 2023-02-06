5af88ab83e342ab45f36e9bcdc258ebad94e5431
Amera White <Amera.White@yahoo.com>
2023-02-06 15:53:35 -0600

    adjusted api units

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 	const [weather, setWeather] = useState({});
 	const [loading, setLoading] = useState(false);
 
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=imperial`;
 
 	const fetchWeather = (e) => {
 		e.preventDefault();
